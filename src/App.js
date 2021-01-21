852190fe492b4c27bc600e7d064304b5babfca95
Márcio <marciobwfilho98@gmail.com>
2021-01-21 20:36:57 -0300

    Corrigindo bug do name(agora pegado com ID)

--- src/App.js
+++ src/App.js
@@ -6,6 +6,8 @@ import Label from './components/labelHistory/labelHistory'
 
 function App() {
 
+    
+
     const [transacoes, setTransacoes] = useState([{
         titulo: 'Salário',
         dinheiro: 1520.00,
@@ -15,12 +17,13 @@ function App() {
 
     function sendInfo() {
         const registros = {
-            titulo: document.getElementsByName('titulo').value,
-            dinheiro: document.getElementsByName('dinheiro').value,
-            descricao: document.getElementsByName('descricao').value,
-            data: document.getElementsByName('data').value
+            titulo: document.getElementById('titulo').value,
+            dinheiro: document.getElementById('dinheiro').value,
+            descricao: document.getElementById('descricao').value,
+            data: document.getElementById('data').value
         }
-
+        
+        
         console.log(registros)
         setTransacoes([...transacoes, registros])
     }
@@ -37,10 +40,10 @@ function App() {
 
             
             <div id='cadastrar'>
-                <input placeholder='Nome' name='titulo'/>
-                <input placeholder='Dinheiro' name='dinheiro'/>
-                <input placeholder='Descrição' name='descricao' />
-                <input placeholder='Data' name='data' /><br/>
+                <input placeholder='Nome' name='titulo' id='titulo' />
+                <input placeholder='Dinheiro' name='dinheiro' id='dinheiro' />
+                <input placeholder='Descrição' name='descricao' id='descricao' />
+                <input placeholder='Data' name='data' id='data' /><br/>
 
                 <button type='submit' onClick={sendInfo}>Adicionar Transação</button>
             </div>
