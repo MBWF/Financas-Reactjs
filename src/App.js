b857197f36e4f408a940479c17ff508750347990
Márcio <marciobwfilho98@gmail.com>
2021-01-23 00:17:26 -0300

    Configurando entrada, saida e saldo total

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, { useState } from 'react'
+import React, { useEffect, useState} from 'react'
 import './App.css'
 
 import Totals from './components/totals/total'
@@ -7,34 +7,63 @@ import Label from './components/labelHistory/labelHistory'
 function App() {
 
     const [transacoes, setTransacoes] = useState([])
- 
-    const [entrada, setEntrada] = useState(0)
 
-    // var i
-    // for(i = 0; i < 2 ; i++) {
-    //     console.log(i)
-    //     setEntrada(entrada + transacoes[i].dinheiro)
-        
-    // }
+    const [titulo, setTitulo] = useState('')
+    const [dinheiro, setDinheiro] = useState(0)
+    const [descricao, setDescricao] = useState('')
+    const [data, setData] = useState('')
+
+    const [entrada, setEntrada] = useState(0)
+    const [saida, setSaida] = useState(0)
+    const [saldo, setSaldo] = useState(0)
+
+    useEffect(
+        () => {
+            setEntrada(
+                transacoes
+                    .filter(transacao => transacao.descricao == "entrada")
+                    .map(transacao => transacao.dinheiro)
+                    .reduce((total, current) => total + current, 0)
+                )
+            setSaida(
+                transacoes
+                .filter(transacao => transacao.descricao == "saida")
+                .map(transacao => transacao.dinheiro)
+                .reduce((total, current) => total + current, 0)
+            )
+            setSaldo(entrada - saida)
+        }, [transacoes, entrada, saida]
+    )
+    
+    function somaEntradas(transacoes) {
+        let valores = 0
+        for (transacao in transacoes) {
+            valores += transacao.dinheiro
+        }
+        setEntrada(valores)
+    }
 
     function sendInfo() {
         const registros = {
-            titulo: document.getElementById('titulo').value,
-            dinheiro: document.getElementById('dinheiro').value,
-            descricao: document.getElementById('descricao').value,
-            data: document.getElementById('data').value
+            titulo,
+            dinheiro,
+            descricao,
+            data
         }
-
-        
         
         console.log(registros)
-        setTransacoes([...transacoes, registros])
-
-        document.getElementById('titulo').value=''
-        document.getElementById('dinheiro').value=''
-        document.getElementById('descricao').value='',
-        document.getElementById('data').value=''
+        const aux = [...transacoes, registros]
+        setTransacoes(aux)
     }
+
+
+    const onChangeDinheiro = e => {
+        if(e.target.value) {
+            setDinheiro(parseFloat(e.target.value))
+        }
+    }
+
+
 
     return (
         <>
@@ -43,17 +72,42 @@ function App() {
             </div>
             <div className='total-saldo'>
                 <Totals title='entrada' valor={entrada}/>
-                <Totals title='Saída' valor='800'/>
-                <Totals title='Saldo' valor='400' className='saldo'/>
+                <Totals title='Saída' valor={saida}/>
+                <Totals title='Saldo' valor={saldo} className='saldo'/>
             </div>
-            {transacoes.map(label => <Label key={label} title={label.titulo} money={label.dinheiro} description={label.descricao} dateMoney={label.data} />)}
+            {transacoes.map(label => <Label key={`${Math.random()}`} title={label.titulo} money={label.dinheiro} description={label.descricao} dateMoney={label.data} />)}
 
             
             <div id='cadastrar'>
-                <input placeholder='Nome' name='titulo' id='titulo' />
-                <input placeholder='Dinheiro' name='dinheiro' id='dinheiro' />
-                <input placeholder='Descrição' name='descricao' id='descricao' />
-                <input placeholder='Data' name='data' id='data' /><br/>
+                <input 
+                    placeholder='Nome' 
+                    name='titulo' 
+                    value={titulo}
+                    onChange={e => setTitulo(e.target.value)}
+                />
+                <input 
+                    placeholder='Dinheiro'
+                    name='dinheiro'
+                    type="number"
+                    value={dinheiro}
+                    onChange={e => {
+                        if(e.target.value) {
+                            setDinheiro(parseFloat(e.target.value))
+                        }
+                    }}
+                />
+                <input
+                    placeholder='Descrição'
+                    name='descricao'
+                    value={descricao}
+                    onChange={e => setDescricao(e.target.value)}
+                />
+                <input
+                    placeholder='Data'
+                    name='data'
+                    value={data}
+                    onChange={e => setData(e.target.value)}
+                /><br/>
 
                 <button type='submit' onClick={sendInfo}>Adicionar Transação</button>
             </div>
